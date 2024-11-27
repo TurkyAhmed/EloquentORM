a89a8d370c8fe74035fed5cef8c9aa8858bcc248
Turky Ahmed <tasg1818@gmail.com>
2024-11-28 00:53:54 +0300

    Most common model variables

--- app/Models/Plog.php
+++ app/Models/Plog.php
@@ -7,10 +7,24 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Plog extends Model
 {
-    //
+    /* To use Soft Delete */
     // use SoftDeletes ;
-    // public $primarKey = 'uuid';
+
+    /* If table name not standared */
+    protected $table="Table_Name";
+
+    /* If primary key not Id */
+    protected $primarKey= 'Column_Name';
+
+    /* If primary key not Id, and type of primary key is string */
+    protected $keyType = 'string';
 
     protected $fillable = ["title", "subTitle"];
+
+    /* If primary key not increment by 1 */
+    public $increment = false;
+
+    /* if you not need to use timestamps */
+    public $timestamps = false;
 
 }
